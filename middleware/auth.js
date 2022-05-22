a949753a211a1e9788ebeb6444590317ceee1551
Fable1611 <96782874+Fable1611@users.noreply.github.com>
2022-05-22 22:18:42 +0200

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,8 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, "TOKEN");
     const userId = decodedToken.userId;
+    req.auth = { userId };
+
     if (req.body.userId && req.body.userId !== userId) {
       throw "user Id non valable !";
     } else {
